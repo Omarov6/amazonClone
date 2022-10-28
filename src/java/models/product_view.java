b5a7196bf9228467280d4d1e2445ae6a2bbdf7bf
Omarv6 <omar15.ov6@gmail.com>
2022-10-28 16:40:29 -0600

    fixing problem with stock

--- src/java/models/product_view.java
+++ src/java/models/product_view.java
@@ -4,7 +4,7 @@ public class product_view extends product{
     
     public int cantidad;
     public product_view(int id, String name, String m, String description, double price, int cantidad) {
-        super(id, name, m, description, price);
+        super(id, name, m, description, price, cantidad);
         this.cantidad = cantidad;
     }
 }
